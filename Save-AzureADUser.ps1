233712ed8cbeb27273ea698d23759715d7f85580
Patrik Suter <4562610+patriksuter@users.noreply.github.com>
2022-06-09 15:44:59 +0200

    Update Save-AzureADUser.ps1
    Replace Special chars whit their Base chars

--- Save-AzureADUser.ps1
+++ Save-AzureADUser.ps1
@@ -19,6 +19,28 @@ if (-Not (Get-Module -ListAvailable -Name AzureAD)) {
     }
 }
 
+Function Remove-Chars {
+    param ([String]$src = [String]::Empty)
+    #replace diacritics
+    $normalized = $src.Normalize( [Text.NormalizationForm]::FormD )
+    $sb = new-object Text.StringBuilder
+    $normalized.ToCharArray() | % {
+        if( [Globalization.CharUnicodeInfo]::GetUnicodeCategory($_) -ne [Globalization.UnicodeCategory]::NonSpacingMark) {
+            [void]$sb.Append($_)
+        }
+    }
+    $sb=$sb.ToString()
+    #replace via code page conversion
+    $NonUnicodeEncoding = [System.Text.Encoding]::GetEncoding(850)
+    $UnicodeEncoding = [System.Text.Encoding]::Unicode
+    [Byte[]] $UnicodeBytes = $UnicodeEncoding.GetBytes($sb);
+    [Byte[]] $NonUnicodeBytes = [System.Text.Encoding]::Convert($UnicodeEncoding, $NonUnicodeEncoding , $UnicodeBytes);
+    [Char[]] $NonUnicodeChars = New-Object -TypeName “Char[]” -ArgumentList $($NonUnicodeEncoding.GetCharCount($NonUnicodeBytes, 0, $NonUnicodeBytes.Length))
+    [void] $NonUnicodeEncoding.GetChars($NonUnicodeBytes, 0, $NonUnicodeBytes.Length, $NonUnicodeChars, 0);
+    [String] $NonUnicodeString = New-Object String(,$NonUnicodeChars)
+    $NonUnicodeString
+}
+
 # Create a temporary file to hold the unformatted results of our Get-AzureADUser query
 $TempFile = New-TemporaryFile
 
@@ -70,7 +92,7 @@ ForEach ($azureADUser in $azureADUsers){
 
         $xmlWriter.WriteElementString("UserPrincipalName",$($azureADUser.UserPrincipalName))
         $xmlWriter.WriteElementString("ObjectId",$($azureADUser.ObjectId))
-        $xmlWriter.WriteElementString("DisplayName",$($azureADUser.DisplayName))
+        $xmlWriter.WriteElementString("DisplayName",$(Remove-Chars $azureADUser.DisplayName))
 
     $xmlWriter.WriteEndElement()
     }
